33a1952243756f131b2d3c20712682c71391bfb8
MiguelIbarra2021 <mi36321@dinuba.k12.ca.us>
2025-04-27 07:39:52 -0700

    Added naive bayes hyperparameter search

--- hyperparameter_search.py
+++ hyperparameter_search.py
@@ -1,8 +1,10 @@
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
-from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
+from scipy.stats import uniform
 from sklearn.pipeline import Pipeline
 from sklearn.exceptions import ConvergenceWarning
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -64,11 +66,13 @@ def review_search():
     # Train/test split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    lr_results = LR(X_train, X_test, y_train, y_test)
     knn_results = KNN(X_train, X_test, y_train, y_test)
+    nb_results = NB(X_train, X_test, y_train, y_test)
+    lr_results = LR(X_train, X_test, y_train, y_test)
 
     combined_results = {
     "Logistic Regression": lr_results,
+    "Naive Bayes": nb_results,
     "KNN": knn_results
     }
 
@@ -145,38 +149,6 @@ def MNIST_search():
 
     print(f"✅ Saved combined results to {output_path}")
 '''
-    
-def LR(X_train, X_test, y_train, y_test):
-    lr_pipeline = Pipeline([
-        ('classifier', LogisticRegression(solver='saga', max_iter=max_iter))
-    ])
-
-    lr_param_dist = {
-        'classifier__C': loguniform(1e-4, 1e2),
-        'classifier__tol': loguniform(1e-4, 1e-1)
-    }
-
-    lr_search = RandomizedSearchCV(
-        lr_pipeline,
-        param_distributions=lr_param_dist,
-        n_iter=n_iterations,
-        cv=cv_folds,
-        n_jobs=-1
-    )
-
-    print("\nFitting Logistic Regression with RandomizedSearchCV...")
-    with tqdm_joblib(tqdm.tqdm(total=n_iterations * cv_folds, desc="LR Search Progress")):
-        lr_search.fit(X_train, y_train)
-
-    print("Best LR Accuracy:", lr_search.score(X_test, y_test))
-    print("Best LR Params:", lr_search.best_params_)
-
-    results = {
-        "best_accuracy": lr_search.score(X_test, y_test),
-        "best_params": lr_search.best_params_
-    }
-
-    return results 
 
 def KNN(X_train, X_test, y_train, y_test):
     # --- Randomized Search for KNN ---
@@ -212,6 +184,72 @@ def KNN(X_train, X_test, y_train, y_test):
 
     return results 
    
+def NB(X_train, X_test, y_train, y_test):
+    # --- Randomized Search for MultinomialNB ---
+    nb_pipeline = Pipeline([
+        ('classifier', MultinomialNB())
+    ])
+
+    # Parameters for MultinomialNB
+    nb_param_dist = {
+        'classifier__alpha': uniform(0.0, 1.0)
+    }
+
+    nb_search = RandomizedSearchCV(
+        nb_pipeline,
+        param_distributions=nb_param_dist,
+        n_iter=10,
+        cv=5,
+        n_jobs=-1,
+        random_state=42
+    )
+
+    print("\nFitting NB with RandomizedSearchCV...")
+    with tqdm_joblib(tqdm.tqdm(total=n_iterations * cv_folds, desc="NB Search Progress")):
+        nb_search.fit(X_train, y_train)
+
+    print("Best NB Accuracy:", nb_search.score(X_test, y_test))
+    print("Best NB Params:", nb_search.best_params_)
+
+    results = {
+        "best_accuracy": nb_search.score(X_test, y_test),
+        "best_params": nb_search.best_params_
+    }
+
+    return results
+
+def LR(X_train, X_test, y_train, y_test):
+    lr_pipeline = Pipeline([
+        ('classifier', LogisticRegression(solver='saga', max_iter=max_iter))
+    ])
+
+    lr_param_dist = {
+        'classifier__C': loguniform(1e-4, 1e2),
+        'classifier__tol': loguniform(1e-4, 1e-1)
+    }
+
+    lr_search = RandomizedSearchCV(
+        lr_pipeline,
+        param_distributions=lr_param_dist,
+        n_iter=n_iterations,
+        cv=cv_folds,
+        n_jobs=-1
+    )
+
+    print("\nFitting Logistic Regression with RandomizedSearchCV...")
+    with tqdm_joblib(tqdm.tqdm(total=n_iterations * cv_folds, desc="LR Search Progress")):
+        lr_search.fit(X_train, y_train)
+
+    print("Best LR Accuracy:", lr_search.score(X_test, y_test))
+    print("Best LR Params:", lr_search.best_params_)
+
+    results = {
+        "best_accuracy": lr_search.score(X_test, y_test),
+        "best_params": lr_search.best_params_
+    }
+
+    return results 
+
 def main():
     #MNIST_search()
     #EMNIST_search()
